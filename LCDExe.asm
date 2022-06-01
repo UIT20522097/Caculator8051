fb3613ba4cb3c2c791ffee2cf47ff8f692cba5af
weedeagle <93306902+weedeagle@users.noreply.github.com>
2022-06-01 22:59:24 +0700

    Update LCDExe.asm

--- LCDExe.asm
+++ LCDExe.asm
@@ -12,8 +12,10 @@ EN		EQU	P0.2
 
       org   0100h
 Start:	
+	 MOV	70H, #0
+Init:
 	 MOV 	A, #0FFH	; Loads A with all 1's
-	 MOV 	P2, #0H		; Initializes P2 as output port
+	 MOV 	P2, #00H	; Initializes P2 as output port
 
 	 MOV	A, #01H		; Clear display
 	 CALL	WriteCmd	
@@ -23,26 +25,23 @@ Start:
 	 CALL	WriteCmd
 	 MOV	A, #06H		; Auto increment mode, i.e., when we send char, cursor position moves right
 	 CALL	WriteCmd
-	 CALL	DELAY
+	 CALL	Delay
 Main:
-	 MOV	A, #'1'
+	 MOV	A, 70H
+	 MOV	DPTR, #Numbers
+	 MOVC	A, @A+DPTR
 	 CALL	WriteData
-	 MOV	A, #'2'
-	 CALL	WriteData
-	 MOV	A, #'3'
-	 CALL	WriteData
-	 MOV	A, #'4'
-	 CALL	WriteData
-	 MOV	A, #'5'
-	 CALL	WriteData
-	 SJMP	Main
+	 CALL	Delay
+	 INC	70H
+	 CJNE	A, #10, Main
+	 SJMP 	Start
 	 
 WriteCmd:	
 	MOV 	P2, A	; Load OUT_PORT with contents of A
 	CLR 	RS				; RS = 0 for command
 	CLR 	RW				; RW = 0 for write
 	SETB 	EN				; EN = 1 for high pulse
-	CALL	DELAY			; Call DELAY subroutine
+	CALL	Delay			; Call DELAY subroutine
 	CLR 	EN				; EN = 0 for low pulse
 	RET
 WriteData: 	
@@ -50,7 +49,7 @@ WriteData:
 	SETB 	RS				; RS = 1 for data
 	CLR 	RW				; RW = 0 for write
 	SETB 	EN				; EN = 1 for high pulse
-	CALL	DELAY			; Call DELAY subroutine
+	CALL	Delay			; Call DELAY subroutine
 	CLR 	EN				; EN = 0 for low pulse
 	RET
 Delay: 	MOV 	R3, #255
@@ -58,5 +57,8 @@ L2: 	MOV 	R4, #255
 L1: 	DJNZ 	R4, L1
 	DJNZ 	R3, L2
 	RET
+	
+Numbers:	DB	'0','1','2','3','4','5','6','7','8','9'
+;Operations:	DB	'+','-','x','/'
 ;====================================================================
       END
