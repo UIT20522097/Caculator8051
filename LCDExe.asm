bb5497964c7dc1035fe665f80014453135ab330c
weedeagle <93306902+weedeagle@users.noreply.github.com>
2022-06-09 22:46:30 +0700

    Update LCDExe.asm

--- LCDExe.asm
+++ LCDExe.asm
@@ -1,7 +1,6 @@
 $NOMOD51
 $INCLUDE (8051.MCU)
 RS		EQU	P0.0
-RW		EQU	P0.1
 EN		EQU	P0.2
 
 ;====================================================================
@@ -19,63 +18,48 @@ Init:
 	
 	 MOV	A, #38H		; 8-bit, 2 line, 5x7 dots
 	 CALL	WriteCmd
-	 CALL	Delay
 	 MOV	A, #0EH		; Display ON cursor, ON
 	 CALL	WriteCmd
-	 CALL	Delay
 	 MOV	A, #01H		; Clear display
 	 CALL	WriteCmd
-	 CALL	Delay
-	 MOV	A, #06H		; Auto increment mode, i.e., when we send char, cursor position moves right
+	 
+Error:	
+	 MOV	A, #85H		
 	 CALL	WriteCmd
-	 CALL	Delay
-
-Result:	
-	 MOV	60H, #1
-	 MOV	61H, #2
-	 MOV	62H, #3
-	 MOV	63H, #4
-	 MOV	64H, #5
-	 MOV	65H, #6
-	 MOV	66H, #7
-	 MOV	67H, #8
-	 MOV	68H, #9
-	 MOV	69H, #8
-	 MOV	6AH, #7
-	 MOV	6BH, #6
-	 MOV	6CH, #5
-	 MOV	6DH, #4
-	 MOV	6EH, #3
-	 MOV	6FH, #2
-	 MOV	R0, #60H
-	 MOV	A, #0CFH
+	 MOV	A, #06H		
 	 CALL	WriteCmd
-	 MOV	A, #04H
-	 CALL	WriteCmd
-
-	 MOV	R7, #16
-	 CALL	Execute
-Next:	 
-	 DJNZ	R7, Execute
-Again:	 SJMP	Negative	
-Execute:
-	 MOV	A, @R0
-	 ADD	A, #00110000B
+	 MOV	R6, #5
+	 MOV	R7, #0
+	 MOV	DPTR, #Error1
+Next1:	 
+	 MOV	A, R7
+	 MOVC	A, @A + DPTR
 	 CALL	WriteData
-	 CALL	Delay
-	 INC	R0
-	 SJMP	Next
-Negative:
-	 MOV	A, #0C0H
+	 INC	R7
+	 DJNZ	R6, Next1
+	 MOV	A, #0CH		
 	 CALL	WriteCmd
-	 MOV	A, #00101101B
+	 SJMP	$
+OutRange:
+	 MOV	A, #82H		
+	 CALL	WriteCmd
+	 MOV	A, #06H		
+	 CALL	WriteCmd
+	 MOV	R6, #12
+	 MOV	R7, #0
+	 MOV	DPTR, #OutRange1
+Next2:	 
+	 MOV	A, R7
+	 MOVC	A, @A + DPTR
 	 CALL	WriteData
-	 CALL 	Delay
+	 INC	R7
+	 DJNZ	R6, Next2
+	 MOV	A, #0CH		
+	 CALL	WriteCmd
 	 SJMP	$
 WriteCmd:	
 	MOV 	P2, A	
 	CLR 	RS				; RS = 0 for command
-	CLR 	RW				; RW = 0 for write
 	SETB 	EN				; EN = 1 for high pulse
 	CALL	Delay			; Call DELAY subroutine
 	CLR 	EN				; EN = 0 for low pulse
@@ -83,18 +67,18 @@ WriteCmd:
 WriteData: 	
 	MOV 	P2, A	
 	SETB 	RS				; RS = 1 for data
-	CLR 	RW				; RW = 0 for write
 	SETB 	EN				; EN = 1 for high pulse
 	CALL	Delay			; Call DELAY subroutine
 	CLR 	EN				; EN = 0 for low pulse
 	RET
-Delay: 	MOV 	R3, #255
+Delay: 	MOV 	R3, #50
 L2: 	MOV 	R4, #255	
 L1: 	DJNZ 	R4, L1
 	DJNZ 	R3, L2
 	RET
 	
-
+Error1: DB	'ERROR'
+OutRange1: DB	'OUT OF RANGE'
 ;====================================================================
 Finish:
       END
